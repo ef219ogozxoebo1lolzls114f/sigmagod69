1eac5f9b5502cc09eeae9947e544283953457569
ef219ogozxoebo1lolzls114f <119745291+ef219ogozxoebo1lolzls114f@users.noreply.github.com>
2024-07-23 08:19:14 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,1 @@
-local StarterGui = game:GetService("StarterGui")
-
-StarterGui:SetCore("SendNotification", {
-    Title = "Crypt";
-    Text = "Crypt has been Injected!"; 
-    Duration = 3;
-    Callback = function()
-        print("Crypt Injected!")
-    end;
-    OkButton = "OK"; 
-})
+print("Crypt Injected")
